b6eaa392a4c6b92eb620daad8b03612667c2110e
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-04 16:13:05 +0100

    chore: add shader GLSL compilation script

---
+++ src/compile.sh
@@ -0,0 +1,2 @@
+/home/$USER/vulkan-sdk/1.4.313.0/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/home/$USER/vulkan-sdk/1.4.313.0/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
